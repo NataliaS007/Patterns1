c7c7fa7db8468b785f29966b801d25c19515306f
Natalia <natalia.semyonova@gmail.com>
2023-07-26 21:08:53 +0300

    Changed code

--- src/test/java/test/FormOfDeliveryCardTest.java
+++ src/test/java/test/FormOfDeliveryCardTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,106 +41,60 @@ class FormOfDeliveryCardTest {
     @Test
     void shouldSuccessfulPlanAndReplanMeeting() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 4;
-        int daysToAddForSecondMeeting = 7;
-        $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='name'] .input__control").setValue(validUser.getName());
-        $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
-        $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForSecondMeeting)));
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Перепланировать?"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $$("button").find(Condition.exactText("Перепланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.text(DataGenerator.generateDate(daysToAddForSecondMeeting)));
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $x("//span[contains(text(),'Перепланировать')]").shouldBe(appear, Duration.ofSeconds(3)).click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, secondMeetingDate);
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $x("//span[contains(text(),'Перепланировать')]").click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
-
-    @Test
-    void shouldWarnIfPlanAndReplanDateEqual() {
-        val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 4;
-        int daysToAddForSecondMeeting = daysToAddForFirstMeeting;
-        $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='name'] .input__control").setValue(validUser.getName());
-        $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
-        $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForSecondMeeting)));
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-    }
-
 
     @Test
     void shouldTestSuccessOrderIfPlusThreeDays() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
-        $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='name'] .input__control").setValue(validUser.getName());
-        $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
-        $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
+        var daysToAddForFirstMeeting = 3;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
 
     @Test
     void shouldTestSuccessOrderIfPlus365Days() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 365;
-        $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForFirstMeeting)));
-        $("[data-test-id='name'] .input__control").setValue(validUser.getName());
-        $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
-        $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
+        var daysToAddForFirstMeeting = 365;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
     @Test
     void shouldTestUnsuccessOrderIfPlusTwoDays() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 2;
+        var daysToAddForFirstMeeting = 2;
         $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -148,16 +103,14 @@ class FormOfDeliveryCardTest {
         $("[data-test-id='name'] .input__control").setValue(validUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Заказ на выбранную дату невозможен"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='date'] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
     }
-
 
     @Test
     void shouldTestUnsuccessOrderIfNoName() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -165,15 +118,14 @@ class FormOfDeliveryCardTest {
                         DataGenerator.generateDate(daysToAddForFirstMeeting)));
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Поле обязательно для заполнения"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldTestUnsuccessOrderIfNoCity() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
                 .sendKeys(Keys.chord(BACK_SPACE,
@@ -181,16 +133,14 @@ class FormOfDeliveryCardTest {
         $("[data-test-id='name'] .input__control").setValue(validUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Поле обязательно для заполнения"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='city'] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
-
 
     @Test
     void shouldTestUnsuccessOrderIfNoNumber() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -198,11 +148,9 @@ class FormOfDeliveryCardTest {
                         DataGenerator.generateDate(daysToAddForFirstMeeting)));
         $("[data-test-id='name'] .input__control").setValue(validUser.getName());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Поле обязательно для заполнения"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
-
 
     @Test
     void shouldTestUnsuccessOrderIfNoDate() {
@@ -214,15 +162,14 @@ class FormOfDeliveryCardTest {
         $("[data-test-id='name'] .input__control").setValue(validUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Неверно введена дата"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='date'] .input__sub").shouldHave(exactText("Неверно введена дата"));
     }
 
     @Test
     void shouldTestUnsuccessOrderIfCityNotFromList() {
         val invalidUser = DataGenerator.InvalidRegistration.generateInvalidUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='city'] .input__control").setValue(invalidUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -231,16 +178,14 @@ class FormOfDeliveryCardTest {
         $("[data-test-id='name'] .input__control").setValue(invalidUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(invalidUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Доставка в выбранный город недоступна"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='city'] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
-
 
     @Test
     void shouldTestUnsuccessOrderIfNameInEnglish() {
         val invalidUser = DataGenerator.InvalidNameRegistration.generateInvalidUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='city'] .input__control").setValue(invalidUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -249,15 +194,14 @@ class FormOfDeliveryCardTest {
         $("[data-test-id='name'] .input__control").setValue(invalidUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(invalidUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("только русские буквы, пробелы и дефисы"))
-                .shouldBe(Condition.visible);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldTestUnsuccessOrderIfNoClickOnCofirmation() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
         $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
         $("[data-test-id='date'] .input__control")
@@ -265,26 +209,22 @@ class FormOfDeliveryCardTest {
                         DataGenerator.generateDate(daysToAddForFirstMeeting)));
         $("[data-test-id='name'] .input__control").setValue(validUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        String text = $(".checkbox__text").getCssValue("color");
-        assertEquals("rgba(255, 92, 92, 1)", text);
+        $$("button").find(exactText("Запланировать")).click();
+        $$(".input__invalid");
     }
 
     @Test
     void shouldTestSuccessOrderIfNameWithSpaceAndHyphen() {
         val validUser = DataGenerator.Registration.generateUser("ru");
-        int daysToAddForFirstMeeting = 3;
+        var daysToAddForFirstMeeting = 3;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control")
-                .sendKeys(Keys.chord(BACK_SPACE,
-                        DataGenerator.generateDate(daysToAddForFirstMeeting)));
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(BACK_SPACE, firstMeetingDate));
         $("[data-test-id='name'] .input__control").setValue("Гребенькова-Ушакова Мария-Анжела");
         $("[data-test-id='phone'] .input__control").setValue(validUser.getPhone());
         $$(".checkbox__box").find(Condition.visible).click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована"))
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $$("button").find(exactText("Запланировать")).click();
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
